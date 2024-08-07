64cf81e2f7c245a270b1acd0e482ec317a4c44e5
ratan-17 <rkmule2002@gmail.com>
2024-08-07 20:11:06 +0530

    Ratan

--- Printingeven.java
+++ Printingeven.java
@@ -1,32 +1,36 @@
+public class Printingeven
+{
+		void meth1(String s)
+		{
+			String [] word=s.split(" "); //splitting every word By "_"(space)	
 
-public class Printingeven {
-public void dynamic(String s) {
-	//Splitting
-	String []words=s.split(" ");
-	//Feeding Each word into one Array	
-	int[]wordsLength=new int[words.length];
-	
-	for(int i=0;i<words.length;i++) {
-		wordsLength[i]=words[i].length();
-	}
-	for(int i=0;i<words.length;i++) {
-		if(wordsLength[i]%2==0) {
-			System.out.println(words[i]+" "+words[i].length());
+			int [] wordsLength= new int [word.length];
+			//
+
+			for (int i=0;i<=word.length-1;i++)
+			{
+				wordsLength[i]=word[i].length(); 
+				//[Christopher , Nolan , is , the , director , of , Oppenheimer] =>string type of aaray
+				//[11          ,5      , 2  , 3   , 8        , 2  ,  11 ] => integer type of array
+			}
+
+			for (int i=0;i<=word.length-1;i++)
+			{
+				if (wordsLength[i]%2==0)
+				{
+				//	System.out.println(word[i] +" : "+word[i].length());
+					System.out.println(word[i] +" : "+wordsLength[i]);
+				}
+			}
 		}
+
+
+	public static void main(String[] args) 
+	{
+		String s="Christopher Nolan is the director of Oppenheimer";
+
+
+		Printingeven obj=new Printingeven();
+		obj.meth1(s);
 	}
-}
-public void statical(String s) {
-	for(String word:s.split(" ")) {
-		if(word.length()%2==0) {
-			System.out.println(word+" "+word.length());
-		}
-	}
-}
-	
-	public static void main(String[] args) {
-	System.out.println("Even Length words");
-	String s="Oppenhimmer introduced Atomic Bomb";
-   Printingeven p=new Printingeven();
-   p.dynamic(s);
-}
 }
